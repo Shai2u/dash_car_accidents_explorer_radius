80edfcfb8876c45c5efba98073ce0af45d6b426e
Shai Sussman <shai.sussman@gmail.com>
2025-04-12 14:10:31 +0300

    Add geojson layer to leaflet

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from dash import Dash, html
+import pandas as pd
+import geopandas as gpd
+import dash_leaflet as dl
+import json
 
 app = Dash(__name__)
 
+# Convert GeoDataFrame to GeoJSON
+df = pd.read_csv('accidents_2023_processed.csv')
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(x=df['lon'], y=df['lat']), crs="EPSG:4326")
+gdf_json = gdf.copy().__geo_interface__
+
 app.layout = html.Div([
-    html.H1('Welcome to my Dash App!'),
-    html.Div('This is a simple div element.')
+    html.H1('Accidents Map'),
+    dl.Map([
+        dl.TileLayer(),
+        dl.GeoJSON(data=gdf_json, id='accidents-geojson')
+    ], center=[32.0853, 34.7818], zoom=12, style={'width': '100%', 'height': '50vh'})
 ])
 
 if __name__ == '__main__':
+    
+    # Create geometry column from lat/lon
     app.run_server(debug=True)
