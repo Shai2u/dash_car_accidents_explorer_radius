267d86acde603663537f1aa458cc74df717aa812
Shai Sussman <shai.sussman@gmail.com>
2025-04-12 14:12:48 +0300

    Refactor center coordinates in app layout for improved readability

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ app = Dash(__name__)
 df = pd.read_csv('accidents_2023_processed.csv')
 gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(x=df['lon'], y=df['lat']), crs="EPSG:4326")
 gdf_json = gdf.copy().__geo_interface__
-
+center = [32.0853, 34.7818]
 app.layout = html.Div([
     html.H1('Accidents Map'),
     dl.Map([
         dl.TileLayer(),
         dl.GeoJSON(data=gdf_json, id='accidents-geojson')
-    ], center=[32.0853, 34.7818], zoom=12, style={'width': '100%', 'height': '50vh'})
+    ], center=center, zoom=12, style={'width': '100%', 'height': '50vh'})
 ])
 
 if __name__ == '__main__':
